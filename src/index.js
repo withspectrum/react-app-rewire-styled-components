c3be1a9124852d6f557f41fe80208f88a1ff20a3
Andrey Melikhov <melikhov@yamoney.ru>
2017-08-11 12:33:12 +0300

    This commit fixes #3

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ function rewireStyledComponents(
   env,
   styledComponentsPluginOptions = {}
 ) {
-  const babelLoader = config.module.rules.find(
-    rule => rule.loader && rule.loader.indexOf('babel-loader') > 0
-  );
+  let babelLoader ;
+  const checkRule = rule => rule.loader && rule.loader.indexOf('babel-loader') > 0;
+  config.module.rules.every(rule => {
+    if (rule.oneOf) {
+      babelLoader = (rule.oneOf.find(checkRule))
+    } else if (checkRule(rule)) {
+      babelLoader = rule.loader;
+    }
+    return !babelLoader;
+  });
+
   babelLoader.options.plugins = (babelLoader.options.plugins || [])
     .concat([['styled-components', styledComponentsPluginOptions]]);
 
